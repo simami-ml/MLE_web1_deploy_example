206725861580e5509f0a1c094821eb02d3e7d7f1
simami <simami@mail.ru>
2024-05-26 13:24:33 +0300

    add endpoint is_ready

--- online_inference/app.py
+++ online_inference/app.py
@@ -79,7 +79,20 @@ def is_ready():
         content = jsonable_encoder({"Message": msg})
     )
 
+@app.get('/is_ready')
+def is_ready():
+    if model_lgbm:
+        status_code = 200
+        msg = "Model is ready"
+    else:
+        status_code = 500
+        msg = "Model not found"
+    
+    return JSONResponse(
+        status_code = status_code,
+        content = jsonable_encoder({"Message": msg})
 
+    )
 
 # Функция, срабатывающая при ошибке парсинга данных
 # (если данные в post запросе имеют неправильный формат)
